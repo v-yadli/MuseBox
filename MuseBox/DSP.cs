59443893913f8e55f02f303ec520211fd077006f
Yatao Li <glocklee@gmail.com>
2012-10-10 23:27:26 +0800

    DSP dependency framework finished. Need testing.

--- MuseBox/DSP.cs
+++ MuseBox/DSP.cs
@@ -10,21 +10,101 @@ namespace MuseBox
     /// In MuseBox, we'll treat everything, from audio signals, to
     /// control signals and MIDI notes as digital signal.
     /// </summary>
+    class EndPoint
+    {
+        public float Data;
+    }
     abstract unsafe class DSP
     {
-        public virtual void Update();
-        internal float CurrentOutput;
+        public DSP(int InputChannelCount, int OutputChannelCount)
+        {
+            InputChannels = new EndPoint[InputChannelCount];
+            OutputChannels = new EndPoint[OutputChannelCount];
+            InputChannelProviders = new DSP[InputChannelCount];
+            for (int i = 0; i < OutputChannelCount; ++i)
+            {
+                OutputChannels[i] = new EndPoint();
+            }
+        }
+        public void PlugInput(int channel, DSP sourceDsp, int sourceChannel)
+        {
+            InputChannels[channel] = sourceDsp.OutputChannels[sourceChannel];
+            InputChannelProviders[channel] = sourceDsp;
+            Hardware.InstallDependency(this, sourceDsp);
+        }
+        public void UnplugInput(int channel)
+        {
+            Hardware.RemoveDependency(InputChannelProviders[channel], this);
+            InputChannels[channel] = null;
+            InputChannelProviders[channel] = null;
+        }
+
+        public void OnInputDeviceRemoved(DSP dsp)
+        {
+            for (int i = 0; i < InputChannels.Length; ++i)
+                if (InputChannelProviders[i] == dsp)
+                {
+                    InputChannelProviders[i] = null;
+                    InputChannels[i] = null;
+                }
+        }
+
+        public float ReadInput(int channel)
+        {
+            if (InputChannels[channel] != null)
+                return InputChannels[channel].Data;
+            else return 0;
+        }
+        public void WriteOutput(int channel, float value)
+        {
+            OutputChannels[channel].Data = value;
+        }
+        private EndPoint[] InputChannels;
+        private EndPoint[] OutputChannels;
+        private DSP[] InputChannelProviders;
+        public abstract void Update();
     }
-    static class DSPManager
+    class AudioInterfaceInput : DSP
     {
-        public static bool InstallDependency(DSP source, DSP target)
+        public AudioInterfaceInput(int channelCount)
+            : base(0, channelCount) // Input device output data
         {
-            return true;
         }
-        public static bool RemoveDependency(DSP source, DSP target)
+        public override void Update()
         {
-            return true;
+            //Nothing, and won't execute
         }
-        private static long dspSeqNum = 0;
+    }
+    class AudioInterfaceOutput : DSP
+    {
+        public AudioInterfaceOutput(int channelCount)
+            : base(channelCount, 0) // Output device eat data
+        {
+        }
+        public override void Update()
+        {
+            //Nothing, and won't execute
+        }
+    }
+
+    class Delay : DSP
+    {
+        public Delay()
+            : base(1, 1)
+        {
+        }
+        public override void Update()
+        {
+            float input = ReadInput(0);
+            float delay = delayBuffer[(currentCursor + 1) % delayBuffer.Length];
+            float output = input * DryRatio + delay * (1.0F - DryRatio);
+            delayBuffer[currentCursor] = output * Damping;
+            WriteOutput(0, output);
+            currentCursor = (currentCursor + 1) % delayBuffer.Length;
+        }
+        private float[] delayBuffer = new float[20480];
+        int currentCursor = 0;
+        private float DryRatio = 0.5F;
+        private float Damping = 0.7F;
     }
 }
--- MuseBox/Program.cs
+++ MuseBox/Program.cs
@@ -12,12 +12,23 @@ namespace MuseBox
         [STAThread]
         static void Main(string[] args)
         {
-            HardwareInterface.StartAudio();
-            while (true)
-            {
-                Thread.Sleep(1000);
-            }
-            HardwareInterface.StopAudio();
+            Hardware.StartAudio();
+            Delay l = new Delay();
+            Delay r = new Delay();
+
+            Hardware.InstallDSP(l);
+            Hardware.InstallDSP(r);
+
+            Hardware.AudioOutput.PlugInput(0, l, 0);
+            Hardware.AudioOutput.PlugInput(1, r, 0);
+            l.PlugInput(0, Hardware.AudioInput, 0);
+            r.PlugInput(0, Hardware.AudioInput, 1);
+            Console.ReadKey();
+            Hardware.StopAudio();
+            Console.ReadKey();
+            Hardware.StartAudio();
+            Console.ReadKey();
+            Hardware.StopAudio();
         }
     }
 }
